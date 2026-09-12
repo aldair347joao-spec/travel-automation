92ea65033986e6b20737da41a7f8f88083baebbf
aldair347joao-spec <aldair347joao@gmail.com>
2026-09-12 23:30:15 +0100

    Create site-adapter.js

---
+++ src/services/site/site-adapter.js
@@ -0,0 +1,78 @@
+class SiteAdapter {
+  async initialize() {
+    throw new Error(
+      "initialize() not implemented"
+    );
+  }
+
+  async login() {
+    throw new Error(
+      "login() not implemented"
+    );
+  }
+
+  async fillApplication() {
+    throw new Error(
+      "fillApplication() not implemented"
+    );
+  }
+
+  async requestOtp() {
+    throw new Error(
+      "requestOtp() not implemented"
+    );
+  }
+
+  async submitOtp() {
+    throw new Error(
+      "submitOtp() not implemented"
+    );
+  }
+
+  async verifyIdentity() {
+    throw new Error(
+      "verifyIdentity() not implemented"
+    );
+  }
+
+  async openCalendar() {
+    throw new Error(
+      "openCalendar() not implemented"
+    );
+  }
+
+  async checkAvailability() {
+    throw new Error(
+      "checkAvailability() not implemented"
+    );
+  }
+
+  async selectSlot() {
+    throw new Error(
+      "selectSlot() not implemented"
+    );
+  }
+
+  async continueApplication() {
+    throw new Error(
+      "continueApplication() not implemented"
+    );
+  }
+
+  async getReference() {
+    throw new Error(
+      "getReference() not implemented"
+    );
+  }
+
+  async getEntity() {
+    throw new Error(
+      "getEntity() not implemented"
+    );
+  }
+
+  async close() {}
+}
+
+module.exports =
+  SiteAdapter;
